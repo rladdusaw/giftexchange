728b08f19381ea5281f7f8c218901d983a293c11
Ryan Laddusaw <rladdusaw@gmail.com>
2016-06-24 14:33:35 -0500

    added url configs for accounts

---
+++ accounts/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
+from . import views as account_views
+
+urlpatterns = [
+	url(
+		r'^login/$',
+		auth_views.login,
+		{'template_name': 'login.html'},
+		name='login_view'
+	),
+	url(r'^logout/$', account_views.LogoutView.as_view(), name='logout_view'),
+	url(r'^register/$', account_views.RegisterView.as_view(), name='register_view'),
+	url(r'^error/$', account_views.ErrorView.as_view(), name='error_view'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView, View
+from django.views.generic.edit import FormView
 
-# Create your views here.
+
+class ErrorView(TemplateView):
+    template_name = 'error.html'
+
+
+class LogoutView(View):
+	
+    def get(self,request):
+        logout(request)
+        return redirect('home')
+
+class RegisterView(FormView):
+
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = '/error/'
+
+    def form_valid(self, form):
+        user = form.save()
+        return redirect('login_view')

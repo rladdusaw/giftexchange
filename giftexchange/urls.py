57adca6f185a5593d7fae7a15387195c6b2fbd0d
Ryan Laddusaw <rladdusaw@gmail.com>
2016-06-24 14:04:03 -0500

    added template directory and included account urls in main url file

--- giftexchange/urls.py
+++ giftexchange/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from accounts import urls as account_urls
+
+from . import views as home_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+	url(r'^$', home_views.HomeView.as_view(), name='home'),
+	url(r'^/accounts/$', include(account_urls)),
 ]

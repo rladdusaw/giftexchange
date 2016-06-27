413a3c15336ce85e0820f563c0a492c34b2ca445
Ryan Laddusaw <rladdusaw@gmail.com>
2016-06-27 10:48:45 -0700

    moved account templates into their own folder

--- giftexchange/settings.py
+++ giftexchange/settings.py
@@ -57,7 +57,11 @@ ROOT_URLCONF = 'giftexchange.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
+        'DIRS': [
+            'templates',
+            'templates/accounts',
+            'templates/wishlist',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
